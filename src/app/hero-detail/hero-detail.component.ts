7b35a164d42ec941c416407c3d1cb07271e5588a
ChenLin12138 <scorpion_chenlin@163.com>
2020-03-07 18:17:57 +0800

    创建特性组件

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+
+  //hero需要点一个@Input属性，因为外部的HeroesComponent组件将会绑定它。
+  //因为他的值是使用hero-detail的父组件传递进来的
+  @Input() hero : Hero;
 
   constructor() { }
 
