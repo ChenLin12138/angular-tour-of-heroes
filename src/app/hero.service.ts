f1578f1a0e1671a2d546fada80b7374cb4fee10a
ChenLin12138 <scorpion_chenlin@163.com>
2020-03-09 21:34:20 +0800

    根据名字搜索

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,19 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
 
+  searchHeroes(term : string) : Observable<Hero[]>{
+    if(!term.trim()){
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ? 
+        this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+        catchError(this.handleError<Hero[]>('searchHeroes',[]))
+    );
+  }
+
   deleteHero(hero : Hero | number ): Observable<Hero> {
     const id = typeof hero === 'number' ? hero : hero.id;
     const url = `${this.heroesUrl}/${id}`;
