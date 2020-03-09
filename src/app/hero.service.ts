7d8b23eb2eaaf5ffdde297c65b125be646ddb95a
ChenLin12138 <scorpion_chenlin@163.com>
2020-03-09 20:57:12 +0800

    从服务获取数据，添加英雄

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,13 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
   
+  addHero(hero: Hero) : Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero : Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+  
   httpOptions = { headers : new HttpHeaders({'Content-Type' : 'application/json'})};
   
   updateHero(hero: Hero) : Observable<any>{
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -32,4 +32,12 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name : string): void{
+    name = name.trim();
+    if(!name.trim()) {return;}
+    this.heroService.addHero({ name } as Hero).subscribe(hero => {
+      this.heroes.push(hero);
+    })
+  }
+
 }
