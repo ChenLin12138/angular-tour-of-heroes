c14e90572ac594f71fe9508df6033972b2b5f305
ChenLin12138 <scorpion_chenlin@163.com>
2020-03-09 20:32:57 +0800

    从服务器获取数据，修改英雄

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { catchError, map, tap } from 'rxjs/operators';
 
 //这个类似Spring中的@Component声明
 @Injectable({
@@ -10,18 +11,49 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
   
-  getHero(id: number) : Observable<Hero> {
-    this.messageService.add(`HeroService: fetch hero id = ${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+  httpOptions = { headers : new HttpHeaders({'Content-Type' : 'application/json'})};
+  
+  updateHero(hero: Hero) : Observable<any>{
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+    .pipe(tap(_=>this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero')));
   }
+
+  
+  
+  //url for web api
+  private heroesUrl = 'api/heroes';
 
   //注入消息服务
   //以公共属性注入,因为我们会在模板中绑定它
-  constructor(public messageService : MessageService ) { }
+  constructor(public messageService : MessageService, private http : HttpClient ) { }
 
+
+  getHero(id: number) : Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(tap(_=>this.log(`fetched hero id = ${id}`)),
+    catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  
   getHeroes() : Observable<Hero[]> {
     this.messageService.add('HeroService: Fetch heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(tap(_=> this.log('fetched heroes')),
+    catchError(this.handleError<Hero[]>('getHeroes',[])));
+  }
+
+  handleError<T>(operation = 'operation', result ? : T) {
+    return (error : any) : Observable<T> =>{
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
+
+  private log(message : string){
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 }
